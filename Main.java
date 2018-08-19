af92c4ebaf552b05f691b134861d964940e96d7d
adicomdotir <adicom.ir@gmail.com>
2018-08-19 10:08:33 +0430

    Add predicate sample

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-import io.reactivex.*;
 import java.util.*;
+import java.util.function.*;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,25 +12,43 @@ public class Main {
 		numberCollection.add(1285);
 
 		numberCollection.stream().filter((x) -> x > 1300).forEach(System.out::println);
+
 		printLine();
-		numberCollection.stream().map((x) -> x *2).forEach(System.out::println);
+
+		numberCollection.stream().map((x) -> x * 2).forEach(System.out::println);
+
 		printLine();
-		numberCollection.stream().map((x) -> x *2).sorted().forEach(System.out::println);
+
+		numberCollection.stream().map((x) -> x * 2).sorted().forEach(System.out::println);
+
 		printLine();
+
 		boolean anyMatch1336 = numberCollection.stream().anyMatch((x) -> x == 1336);
 		System.out.println(anyMatch1336);      // true
 		boolean allMatch1336 = numberCollection.stream().allMatch((x) -> x == 1336);
 		System.out.println(allMatch1336);      // false
 		boolean noneMatch1336 = numberCollection.stream().noneMatch((x) -> x == 1336);
 		System.out.println(noneMatch1336);      // false
+
 		printLine();
+
 		long lowerThan1300 = numberCollection.stream().filter((s) -> s < 1300).count();
 		System.out.println(lowerThan1300);    // 2
+
 		printLine();
+
 		Optional<Integer> reduced = numberCollection.stream().sorted().reduce((x, y) -> x + y);
 		reduced.ifPresent(System.out::println);
+
 		printLine();
-		Flowable.just("Hello world").subscribe(System.out::println);
+
+		Predicate<String> predicate = (s) -> s.length() > 0;
+		System.out.println(predicate.test("foo"));              // true
+		System.out.println(predicate.negate().test("foo"));     // false
+		Predicate<Boolean> nonNull = Objects::nonNull;
+		Predicate<Boolean> isNull = Objects::isNull;
+		Predicate<String> isEmpty = String::isEmpty;
+		Predicate<String> isNotEmpty = isEmpty.negate();
 	}
 
 	public static void printLine() {
