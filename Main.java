32239ad5d189a0377daf1d74cc84d37bf6de17d0
adicomdotir <adicom.ir@gmail.com>
2018-12-30 10:19:04 +0330

    Clean javadoc comment

--- Main.java
+++ Main.java
@@ -3,95 +3,10 @@ import java.util.function.*;
 import java.util.stream.*;
 import java.text.MessageFormat;
 
-/**
- * Brief summary of this class, ending with a period.
- *
- * It is common to leave a blank line between the summary and further details.
- * The summary (everything before the first period) is used in the class or package
- * overview section.
- *
- * The following inline tags can be used (not an exhaustive list):
- * {@link some.other.class.Documentation} for linking to other docs or symbols
- * {@link some.other.class.Documentation Some Display Name} the link's appearance can be
- * customized by adding a display name after the doc or symbol locator
- * {@code code goes here} for formatting as code
- * {@literal <>[]()foo} for interpreting literal text without converting to HTML markup
- * or other tags.
- *
- * Optionally, the following tags may be used at the end of class documentation
- * (not an exhaustive list):
- *
- * @author John Doe
- * @version 1.0
- * @since 5/10/15
- * @see some.other.class.Documentation
- * @deprecated This class has been replaced by some.other.package.BetterFileReader
- *
- * You can also have custom tags for displaying additional information.
- * Using the @custom.<NAME> tag and the -tag custom.<NAME>:htmltag:"context"
- * command line option, you can create a custom tag.
- *
- * Example custom tag and generation:
- * @custom.updated 2.0
- * Javadoc flag: -tag custom.updated:a:"Updated in version:"
- * The above flag will display the value of @custom.updated under "Updated in version:"
- *
- */
 public class Main {
 
-
-	/**
-	 * Fields can be documented as well.
-	 *
-	 * As with other javadocs, the documentation before the first period is used as a
-	 * summary, and is usually separated from the rest of the documentation by a blank
-	 * line.
-	 *
-	 * Documentation for fields can use inline tags, such as:
-	 * {@code code here}
-	 * {@literal text here}
-	 * {@link other.docs.Here}
-	 *
-	 * Field documentation can also make use of the following tags:
-	 *
-	 * @since 2.1.0
-	 * @see some.other.class.Documentation
-	 * @deprecated Describe why this field is outdated
-	 */
 	public int sampleField = 1;
-
-	/**
-	 * Brief summary of method, ending with a period.
-	 *
-	 * Further description of method and what it does, including as much detail as is
-	 * appropriate. Inline tags such as
-	 * {@code code here}, {@link some.other.Docs}, and {@literal text here} can be used.
-	 *
-	 * If a method overrides a superclass method, {@inheritDoc} can be used to copy the
-	 * documentation
-	 * from the superclass method
-	 *
-	 * @param stream Describe this parameter. Include as much detail as is appropriate
-	 * Parameter docs are commonly aligned as here, but this is optional.
-	 * As with other docs, the documentation before the first period is
-	 * used as a summary.
-	 *
-	 * @return Describe the return values. Include as much detail as is appropriate
-	 * Return type docs are commonly aligned as here, but this is optional.
-	 * As with other docs, the documentation before the first period is used as a
-	 * summary.
-	 *
-	 * @throws IOException Describe when and why this exception can be thrown.
-	 * Exception docs are commonly aligned as here, but this is
-	 * optional.
-	 * As with other docs, the documentation before the first period
-	 * is used as a summary.
-	 * Instead of @throws, @exception can also be used.
-	 *
-	 * @since 2.1.0
-	 * @see some.other.class.Documentation
-	 * @deprecated Describe why this method is outdated. A replacement can also be specified.
-	 */
+    
 	public static void main(String[] args) {
 
 		List<Integer> numberCollection = new ArrayList<>();
