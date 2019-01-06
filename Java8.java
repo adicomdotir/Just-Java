63c390637e629e5815b911516624ab841a22d423
adicomdotir <adicom.ir@gmail.com>
2019-01-06 11:35:06 +0330

    Default Method 2

--- Java8.java
+++ Java8.java
@@ -1,14 +1,17 @@
 public class Java8 {
 	public static void main(String[] args) {
-		Formula f = new Formula() {
-
-		};
+		Formula f = new Formula() {};
 		System.out.println(f.print());
 	}
 }
 
-interface Formula {
+interface Parent {
+	default String print() {
+        return "Parent Default Method";
+    }
+}
+interface Formula extends Parent {
     default String print() {
-        return "Default Method";
+        return "Formula Default Method";
     }
 } 
