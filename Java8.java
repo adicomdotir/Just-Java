2c1905df9d7baa549278a8e2d7d342a96004c566
adicomdotir <adicom.ir@gmail.com>
2019-01-08 12:05:46 +0330

    Using method reference

--- Java8.java
+++ Java8.java
@@ -4,7 +4,8 @@ public class Java8 {
 		System.out.println(f.print());
 
 		
-		Converter<String, Integer> converter = (from) -> Integer.valueOf(from);
+		// Converter<String, Integer> converter = (from) -> Integer.valueOf(from);
+		Converter<String, Integer> converter = Integer::valueOf;
 		Integer converted = converter.convert("123");
 		System.out.println(converted);    // 123
 	}
