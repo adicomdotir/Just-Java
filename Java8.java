372e18c3140d37592d668baf5426bdd7b5e7eb33
adicomdotir <adicom.ir@gmail.com>
2019-01-09 12:39:48 +0330

    supplier example

--- Java8.java
+++ Java8.java
@@ -1,13 +1,18 @@
+import java.util.function.*;
+
 public class Java8 {
 	public static void main(String[] args) {
 		Formula f = new Formula() {};
 		System.out.println(f.print());
 
-		
+
 		// Converter<String, Integer> converter = (from) -> Integer.valueOf(from);
 		Converter<String, Integer> converter = Integer::valueOf;
 		Integer converted = converter.convert("123");
 		System.out.println(converted);    // 123
+
+		Supplier<String> strSupplier = String::new;
+		strSupplier.get();
 	}
 }
 
