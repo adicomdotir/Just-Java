3138b7f51e64c022467dd6cdf9273b4044dad950
adicomdotir <adicom.ir@gmail.com>
2019-05-25 15:51:21 +0430

    Add lsp solid sample

---
+++ SOLID/lsp/LSP.java
@@ -0,0 +1,22 @@
+public class LSP {
+	public static void main(String[] args) {
+	}
+}
+
+interface Calc {
+	calculate();
+}
+
+class SumCalc implements Calc {
+	@Override
+	public void calculate() {
+		// calculate
+	}
+}
+
+class EvenSumCalc implements Calc {
+	@Override
+	public void calculate() {
+		// calculate
+	}
+}
---
+++ SOLID/lsp/NoLSP.java
@@ -0,0 +1,17 @@
+public class LSP {
+	public static void main(String[] args) {
+	}
+}
+
+class SumCalc {
+	public void calculate() {
+		// calculate
+	}
+}
+
+class EvenSumCalc extends SumCalc {
+	@Override
+	public void calculate() {
+		// calculate
+	}
+}
