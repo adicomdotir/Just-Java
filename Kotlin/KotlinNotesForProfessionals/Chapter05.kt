426863c28e6176860a7e983cfaf96c009366c594
adicomdotir <adicom.ir@gmail.com>
2019-08-26 11:50:15 +0430

    Complete chapter five

--- Kotlin/KotlinNotesForProfessionals/Chapter05.kt
+++ Kotlin/KotlinNotesForProfessionals/Chapter05.kt
@@ -1,3 +1,13 @@
 fun main(args Array<String>) {
-    
+    // List
+    val list = listOf("Item 1", "Item 2", "Item 3")
+    println(list)
+
+    // Map
+    val map = mapOf(Pair(1, "Item 1"), Pair(2, "Item 2"))
+    println(map)
+
+    // Set
+    val set = setOf(1, 3, 5)
+    println(set)
 }
