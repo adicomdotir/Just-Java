2b90480f85b28105bda6a0e99ff373c728c9784e
adicomdotir <adicom.ir@gmail.com>
2020-05-18 15:54:42 +0430

    Add other loop examples

--- Kotlin/loop.kt
+++ Kotlin/loop.kt
@@ -1,10 +1,57 @@
 fun main(args: Array<String>) {
     val arr = arrayOf(23, 34, 64, 9, 12)
     forExample(arr)
-    whileExample(arr)
+    whileDoWhileExample(arr)
+    breakExample()
+    continueExample()
 }
 
-fun whileExample(arr: Array<Int>): Unit { }
+fun breakExample() {
+    myLoop@ for (i in 1..3) {
+        for (j in 1..3) {
+            println("$i  $j")
+            if (i == 2 && j == 2) {
+                break@myLoop
+            }
+        }
+    }
+    /*  OUTPUT
+        1   1   1   2   1   3
+        2   1   2   2
+    */
+}
+    
+fun continueExample() {
+    myLoop@ for (i in 1..3) {
+        for (j in 1..3) {
+            if (i == 2 && j == 2) {
+                continue@myLoop
+            }
+            println("$i  $j")
+        }
+    }
+    /*  OUTPUT
+        1   1   1   2   1   3
+        2   1
+        3   1   3   2   3   3
+    */
+}
+
+fun whileDoWhileExample(arr: Array<Int>): Unit {
+    println("while()")
+    var index = 0;
+    while(index < arr.size) {
+        print(" ${arr[index]}")
+        index++
+    }
+
+    println("do {} while()")
+    var index = 0;
+    do {
+        print(" ${arr[index]}")
+        index++
+    } while (index <= arr.size)
+}
 
 fun forExample(arr: Array<Int>): Unit {
     println("for (item in arr)")
